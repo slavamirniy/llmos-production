3461b460c634833cc3dbf311120673d01ca65986
Your Name <you@example.com>
2025-02-08 08:46:02 +0300

    upd type exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import './types/os.js';
 import './types/builder.js';
 import './types/jsonschema.js';
 
+export * from './types/base.js';
+export * from './types/os.js';
+export * from './types/builder.js';
+export * from './types/jsonschema.js';
+export * from './types/index.js';
+
 
 
 
